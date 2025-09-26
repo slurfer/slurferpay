6c0da1332898023a6340d5c675900a5107acc58f
Martin Doušek <dousek.martin@gmail.com>
2025-09-26 16:55:58 +0200

    test nfc

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,18 @@
+import { readNfc } from "@/util/nfc";
+import { decodeNfcRecord } from "@/util/nfc";
+
 export default function Home() {
+  readNfc(
+    (message) => {
+      const msgText = decodeNfcRecord(message.records[0]);
+      if (msgText.indexOf(";") < 0) alert("Malformed card data: " + msgText);
+      else {
+        // TODO layout success
+        alert("Card data: " + msgText);
+      }
+    },
+    () => alert("Error while reading card.")
+  );
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1 className="text-4xl font-bold text-center">
---
+++ util/nfc.ts
@@ -0,0 +1,37 @@
+export const readNfc = async (
+  onRead: (message: NDEFMessage, serialNumber: any) => void,
+  onError: () => void
+) => {
+  try {
+    alert("Please tap your NFC card.");
+    const ndef = new NDEFReader();
+    await ndef.scan();
+
+    const onScan = ({ message, serialNumber }: any) => {
+      onRead(message, serialNumber);
+      ndef.removeEventListener("reading", onScan);
+    };
+
+    ndef.addEventListener("reading", onScan);
+  } catch (error) {
+    onError();
+  }
+};
+
+export const writeNfc = async (
+  message: string,
+  onDone: () => void,
+  onError: () => void
+) => {
+  try {
+    const ndef = new NDEFReader();
+    await ndef.write(message);
+    onDone();
+  } catch (error) {
+    onError();
+  }
+};
+
+export const decodeNfcRecord = (record?: NDEFRecord) => {
+  return record ? new TextDecoder(record.encoding).decode(record.data) : "";
+};
